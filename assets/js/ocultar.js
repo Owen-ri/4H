9a05136ffda77b3bfd727bb240b5143574dfeea3
owen <167233883+Owen-ri@users.noreply.github.com>
2024-06-12 10:47:18 -0600

    s

--- assets/js/ocultar.js
+++ assets/js/ocultar.js
@@ -1,17 +1,10 @@
-document.getElementById('showButton').addEventListener('click', function() {
-    var contenido = document.getElementById('contenido');
-    var showButton = document.getElementById('showButton');
-    var hideButton = document.getElementById('hideButton');
-    contenido.style.display = 'block';
-    showButton.style.display = 'none';
-    hideButton.style.display = 'inline';
-});
-
-document.getElementById('hideButton').addEventListener('click', function() {
-    var contenido = document.getElementById('contenido');
-    var showButton = document.getElementById('showButton');
-    var hideButton = document.getElementById('hideButton');
-    contenido.style.display = 'none';
-    showButton.style.display = 'inline';
-    hideButton.style.display = 'none';
-});
+function toggleDivs() {
+    var divs = document.querySelectorAll('.div-box');
+    divs.forEach(function(div) {
+        if (div.classList.contains('hidden')) {
+            div.classList.remove('hidden');
+        } else {
+            div.classList.add('hidden');
+        }
+    });
+}
